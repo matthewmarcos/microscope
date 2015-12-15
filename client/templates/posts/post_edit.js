e985237761aef513e5251b690706c64e1022c783
Matthew <matthewmarcos94@gmail.com>
2015-12-16 03:52:19 +0800

    Editing app checks if URl already exists

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -6,14 +6,26 @@ Template.postEdit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		}
-		Posts.update(currentPostId, {$set: postProperties}, function(error) {
-			if (error) {
-			// display the error to the user
-				alert(error.reason);
-			} else {
-				Router.go('postPage', {_id: currentPostId});
+
+		Meteor.call('postEdit', postProperties, function (err, res) {
+			if(err) {
+				return alert(err.reason);
 			}
+
+			if(res.postExists) {
+				alert('A post with this link has already been posted!');
+			}
+
+			Router.go('postPage', {_id: res._id}); //constructs URL for us to go to.
 		});
+		// Posts.update(currentPostId, {$set: postProperties}, function(error) {
+		// 	if (error) {
+		// 	// display the error to the user
+		// 		alert(error.reason);
+		// 	} else {
+		// 		Router.go('postPage', {_id: currentPostId});
+		// 	}
+		// });
 	},
 	'click .delete': function(e) {
 		e.preventDefault();
