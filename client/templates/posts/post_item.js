55551b7ce168e9d26523ab9f580846ae5a89f33a
Matthew <matthewmarcos94@gmail.com>
2015-12-16 08:18:44 +0800

    Discuss button does not appear on post page

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,5 +7,13 @@ Template.postItem.helpers({
 		a.href = this.url;
 		// this refers to object being iterated on
 		return a.hostname;
+	},
+	commentsCount: function() {
+		return Comments.find({postId: this._id}).count();
+	},
+	alreadyInPage: function() {
+		// console.log(Router.current().route.path(this));
+		// console.log('/posts/' + this._id);
+		return !(Router.current().route.path(this) === '/posts/' + this._id);
 	}
 });
